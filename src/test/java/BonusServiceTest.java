413f616953dcde73e20bc68d016ba713d790d491
Valentin Fadeev <valentinfs@yandex.ru>
2021-08-23 22:45:59 +0300

    Initial commit

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,65 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BonusServiceTest {
+
+    @Test
+    void shouldCalculateRegisteredAndBonusUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, true);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateRegisteredAndBonusOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_000_60;
+        boolean registered = true;
+        long expected = 50;
+
+        long actual = service.calculate(amount, true);
+
+        assertEquals(expected, actual);
+
+
+
+    }
+
+    @Test
+    void shouldCalculateUnRegisteredAndBonusUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, false);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateUnRegisteredAndBonusOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 50;
+
+        long actual = service.calculate(amount, false);
+
+        assertEquals(expected, actual);
+
+    }
+}
